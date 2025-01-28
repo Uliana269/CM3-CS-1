00ffa55aef58347325ce020cca4ba760d373ae1e
Uliana269 <agapovaud@gmail.com>
2025-01-28 03:01:03 +0000

    Pending changes exported from your codespace

---
+++ mario/mario.py
@@ -0,0 +1,54 @@
+while True:
+    answer = input("Choose the number between 1 and 8: ")
+    if answer == "1":
+        print(" #  #")
+        break
+    elif answer == "2":
+        print(" #  #")
+        print("##  ##")
+        break
+    elif answer == "3":
+        print("  #  #")
+        print(" ##  ##")
+        print("###  ###")
+        break
+    elif answer == "4":
+        print("   #  #")
+        print("  ##  ##")
+        print(" ###  ###")
+        print("####  ####")
+        break
+    elif answer == "5":
+        print("    #  #")
+        print("   ##  ##")
+        print("  ###  ###")
+        print(" ####  ####")
+        print("#####  #####")
+        break
+    elif answer == "6":
+        print("     #  #")
+        print("    ##  ##")
+        print("   ###  ###")
+        print("  ####  ####")
+        print(" #####  #####")
+        print("######  ######")
+        break
+    elif answer == "7":
+        print("      #  #")
+        print("     ##  ##")
+        print("    ###  ###")
+        print("   ####  ####")
+        print("  #####  #####")
+        print(" ######  ######")
+        print("#######  #######")
+        break
+    elif answer == "8":
+        print("       #  #")
+        print("      ##  ##")
+        print("     ###  ###")
+        print("    ####  ####")
+        print("   #####  #####")
+        print("  ######  ######")
+        print(" #######  #######")
+        print("########  ########")
+        break
